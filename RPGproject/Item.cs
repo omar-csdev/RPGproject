e535472c470e2f1bf310980297a865d9e5b47e9c
choxkri <1059788@hr.nl>
2023-02-21 13:09:28 +0100

    monster class aangemaakt

--- RPGproject/Item.cs
+++ RPGproject/Item.cs
@@ -1,11 +1,11 @@
 public class Item{
-    public int item_ID;
-    public string item_name;
-    public string item_name_plural;
+    public int ID;
+    public string Name;
+    public string NamePlural;
 
     public Item(int id, string name, string name_plural){
-        this.item_ID = id;
-        this.item_name = name;
-        this.item_name_plural = name_plural;
+        this.ID = id;
+        this.Name = name;
+        this.NamePlural = name_plural;
     }
 }
---
+++ RPGproject/Monster.cs
@@ -0,0 +1,13 @@
+﻿public class Monster
+{
+    public int ID;
+    public string Name;
+    public string NamePlural;
+    public int MaximumDamage;
+    public int RewardExperience;
+    public int RewardGold;
+    public CountedItemList Loot;
+    public int CurrentHitPoints;
+    public int MaximumHitPoints;
+
+}
