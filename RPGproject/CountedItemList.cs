9f4358fab3aa24c6bd1cebad4ebeabcfc4702e04
choxkri <1059788@hr.nl>
2023-02-21 13:45:53 +0100

    tweaks en updates gebracht aan: CountedItemList, Monster

---
+++ RPGproject/CountedItemList.cs
@@ -0,0 +1,34 @@
+﻿public class CountedItemList
+{
+    public List<CountedItem> TheCountedItemList;
+
+    public CountedItemList()
+    {
+        this.TheCountedItemList = new List<CountedItem>();
+    }
+
+    public void AddItem(Item item)
+    {
+        int count = 0;
+        foreach (CountedItem i in this.TheCountedItemList) 
+        {
+            if (i.TheItem.ID == item.ID)
+            {
+                count++;
+            }
+        }
+
+        if (count == 0) 
+        {
+            this.TheCountedItemList.Add(new CountedItem(item));
+        }
+    }
+
+    public void DisplayItems()
+    {
+        foreach (CountedItem item in this.TheCountedItemList) 
+        {
+            Console.WriteLine($"{item.TheItem.Name} x {item.Quantity}");
+        }
+    }
+}
--- RPGproject/Monster.cs
+++ RPGproject/Monster.cs
@@ -3,11 +3,23 @@
     public int ID;
     public string Name;
     public string NamePlural;
-    public int MaximumDamage;
-    public int RewardExperience;
-    public int RewardGold;
+    public int MaximumDamage = 0;
+    public int RewardExperience = 0;
+    public int RewardGold = 0;
     public CountedItemList Loot;
-    public int CurrentHitPoints;
-    public int MaximumHitPoints;
+    public int CurrentHitPoints = 0;
+    public int MaximumHitPoints = 0;
 
+    public Monster(int id, string n, string np, int md, int re, int rg, int c, int mhp)
+    {
+        this.ID = id;
+        this.Name = n;
+        this.NamePlural = np;
+        this.MaximumDamage = md;
+        this.RewardExperience = re;
+        this.RewardGold = rg;
+        this.Loot = new CountedItemList();
+        this.CurrentHitPoints = c;
+        this.MaximumHitPoints = mhp;
+    }
 }
