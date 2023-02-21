f7b6b7966705e88357d8c391394fe2a97ad44fbf
choxkri <1059788@hr.nl>
2023-02-21 14:06:06 +0100

    working on class Location

---
+++ RPGproject/Location.cs
@@ -0,0 +1,23 @@
+﻿public class Location
+{
+    public int ID;
+    public string Name;
+    public string Description;
+    public Item ItemRequiredToEnter;
+    public Quest QuestAvailableHere;
+    public Monster MonsterLivingHere;
+    public Location LocationToNorth = null;
+    public Location LocationToSouth = null;
+    public Location LocationToWest = null;
+    public Location LocationToEast = null;
+
+    public Location(int id, string n, string d, Item item, Quest q, Monster m)
+    {
+        this.ID = id;
+        this.Name = n;
+        this.Description = d;
+        this.ItemRequiredToEnter = item;
+        this.QuestAvailableHere = q;
+        this.MonsterLivingHere = m;
+    }
+}
