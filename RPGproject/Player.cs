335e461cfd49689f7f30ed99d5be7a7fd913d22d
iHesh84 <1058647@hr.nl>
2023-02-21 21:27:04 +0100

    bijna klaar met class player

---
+++ RPGproject/Player.cs
@@ -0,0 +1,11 @@
+﻿public class Player
+{
+    public string Name { get; set; }
+    public int CurrentHitPoints { get; set; }
+    public int MaximumHitPoints { get; set; }
+    public int Gold { get; set; }
+    public int ExperiencePoints { get; set; }
+    public int Level { get; set; }
+    public Weapon CurrentWeapon { get; set; }
+    public Location CurrentLocation { get; set; }
+    public 
