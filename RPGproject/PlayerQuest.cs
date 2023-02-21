4f6f84a952592a0b4e929bc3c81e77a91cd2f83b
choxkri <1059788@hr.nl>
2023-02-21 15:13:50 +0100

    Created PlayerQuest and QuestList classes

---
+++ RPGproject/PlayerQuest.cs
@@ -0,0 +1,15 @@
+﻿public class PlayerQuest
+{
+    public Quest TheQuest;
+    public bool isCompleted;
+
+    public PlayerQuest(Quest theQuest)
+    {
+        this.TheQuest = theQuest;
+    }
+
+    public void CompleteQuest()
+    {
+        this.isCompleted = true;
+    }
+}
---
+++ RPGproject/QuestList.cs
@@ -0,0 +1,26 @@
+﻿public class QuestList
+{
+    public List<PlayerQuest> QuestLog;
+
+    public QuestList()
+    {
+        this.QuestLog = new List<PlayerQuest>();
+    }
+
+    public void AddQuest(PlayerQuest Quest)
+    {
+        int count = 0;
+        foreach (PlayerQuest i in QuestLog) 
+        {
+            if (i.TheQuest.ID == Quest.TheQuest.ID) 
+            {
+                count++;
+            }
+        }
+
+        if (count == 0)
+        {
+            this.QuestLog.Add(Quest);
+        }
+    }
+}
