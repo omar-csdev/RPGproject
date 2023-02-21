3f9857de0e3588dba33998c5e1817286993724ff
Oualid INF1A <1014815@hr.nl>
2023-02-21 02:39:18 +0100

    Ik heb de ITEM class toegevoegd

---
+++ RPGproject/Item.cs
@@ -0,0 +1,11 @@
+class Item{
+    public int item_ID;
+    public string item_name;
+    public string item_name_plural;
+
+    public Item(int id, string name, string name_plural){
+        this.item_ID = id;
+        this.item_name = name;
+        this.item_name_plural = name_plural;
+    }
+}
